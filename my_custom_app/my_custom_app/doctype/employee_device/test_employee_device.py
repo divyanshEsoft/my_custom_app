1399919cadcb22fbe938c73814081418a8357c22
divyansh <divyansh@esoftech.com>
2025-10-07 15:49:31 +0530

    updated the Custom doc type

---
+++ my_custom_app/my_custom_app/doctype/employee_device/test_employee_device.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Divyansh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEmployeeDevice(FrappeTestCase):
+	pass
